c19b563e1cf149729c2518126537843a4d3ebd51
Fonfella <marcello.donzella@gmail.com>
2021-11-09 14:33:23 +0100

    android addons otp yap

--- src/main/Addon/OtpRetrieveYapFonfella.java
+++ src/main/Addon/OtpRetrieveYapFonfella.java
@@ -4,6 +4,7 @@ package main.Addon;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.testproject.java.annotations.v2.Action;
+import io.testproject.java.annotations.v2.Parameter;
 import io.testproject.java.sdk.v2.addons.AndroidAction;
 import io.testproject.java.sdk.v2.addons.WebAction;
 import io.testproject.java.sdk.v2.addons.helpers.AndroidAddonHelper;
@@ -23,35 +24,43 @@ import java.util.ArrayList;
 @Action(name="Otp Retrieve Yap Fonfella")
 public class OtpRetrieveYapFonfella implements AndroidAction {
 
+    @Parameter(defaultValue = "https://stgapi.nexi.it/mfa/getlastotp?user=%2B39")
+    public String startUrl;
+
+    @Parameter(defaultValue = "3486896752")
+    public String numeroTelefono;
+
     @Override
     public ExecutionResult execute(AndroidAddonHelper helper) throws FailureException {
 
 
         AndroidDriver driver = helper.getDriver();
 
-
-        String startUrl = "https://stgapi.nexi.it/mfa/getlastotp?user=%2B39";
-        String url = startUrl + "3486896752"; //telephoneNumber parameter
+//String numeroTelefono = "";
+ //       String startUrl = "https://stgapi.nexi.it/mfa/getlastotp?user=%2B39";
+        String url = startUrl + numeroTelefono; //telephoneNumber parameter
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(url).build();
         try (Response response = client.newCall(request).execute()) {
             String myResponse = response.body().string();
             if (myResponse.contains("Nessun OTP trovato")) {
-           System.out.println("[DETAILS] Nessun OTP Trovato");
+      //     System.out.println("[DETAILS] Nessun OTP Trovato");
             } else {
                 String[] result = myResponse.split("OTP");
                 String[] result1 = result[1].split("</html>");
                 String var = result1[0].trim();
-               System.out.println("[DETAILS] I found OTP: "+var+", for user:") ;
+     //          System.out.println("[DETAILS] I found OTP: "+var+", for user:") ;
             //+telephonNumber);
 
                 driver.findElementById("it.nexi.yap.stg:id/input_password").sendKeys(var);
-                System.out.println("[DETAILS] I use OTP: "+var);
+       //         System.out.println("[DETAILS] I use OTP: "+var);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+      //  String var = "111";
+      //  driver.findElementById("it.nexi.yap.stg:id/input_password").sendKeys(var);
         return ExecutionResult.PASSED;
 
         }
