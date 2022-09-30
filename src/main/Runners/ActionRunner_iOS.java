0909a674d015649e3e4728868492d66825bb7337
mdonzella <marcello.donzella@gmal.com>
2022-09-30 10:29:43 +0200

    runner per esecuzione locale iOS

---
+++ src/main/Runners/ActionRunner_iOS.java
@@ -0,0 +1,39 @@
+package main.Runners;
+
+import io.testproject.java.classes.DriverSettings;
+import io.testproject.java.enums.DriverType;
+import io.testproject.java.sdk.v2.Runner;
+
+import io.testproject.java.sdk.v2.drivers.IOSDriver;
+import io.testproject.java.sdk.v2.internal.GenericCodeBlock;
+import main.Addon.iOS.NPOtpRetrieveIos;
+
+
+public class ActionRunner_iOS {
+
+    private static Runner runner;
+    private static IOSDriver driver;
+
+
+    private final static String devToken = "rgcLJ3OzoYsJZXSgNI-vt73WpAOjnMHzV0tJhjU9xhU1";
+
+    public static void main(String[] args) throws Exception{
+        DriverSettings driverSettings = new DriverSettings(DriverType.Appium_iOS);
+
+        runner = Runner.createIOS(devToken,
+                "00008030-00162D011E9B402E",
+                "iPhone di mdonzella",
+                "it.cartasi.mobile.iPhone.enterprise");
+
+        driver = runner.getDriver();
+        //   try(Runner runner = new Runner(devToken, driverSettings)){
+
+        NPOtpRetrieveIos addoniOS = new NPOtpRetrieveIos();
+
+        IOSDriver driver = runner.getDriver();
+
+        //Click all the menu items of the menu
+        runner.run((GenericCodeBlock) addoniOS);
+
+    }
+}
